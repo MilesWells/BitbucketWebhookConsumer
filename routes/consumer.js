da703919cbd8304bf338cf3c20a97a853c5a53a4
Miles Wells <miles.wells@dudesolutions.com>
2017-11-09 11:38:53 -0500

    socket stuff maybe done

--- routes/consumer.js
+++ routes/consumer.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const baseUri = 'https://code.dudesoln.com/rest/api/1.0/projects';
+
 router.post('/', (req, res) => {
-  console.log(JSON.stringify(req.body));
-  res.json({});
+  let repoName = req.body.repository.fullName;
+  let commitId = req.body.push.changes[0].new.target.hash;
+  let uri = `${baseUri}/${repoName}/commits/${commitId}/changes`;
+
+  let response = {
+      changeUri: uri
+  };
+
+  req.app.io.emit('commit', response);
+
+  res.json(response);
+
 });
 
 module.exports = router;
